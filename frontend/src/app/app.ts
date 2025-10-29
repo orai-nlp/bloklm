3c78de6c0d374fa854d8560c5b6dfc116e4600fd
Asier Garin Aldezabal <a.garin@elhuyar.eus>
2025-10-29 12:38:53 +0100

    Footerra gehitua

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router"
 import { HeaderComponent } from "./components/header/header"
 import { NoteModalComponent } from "./components/note-modal/note-modal"
 import { FileModalComponent } from "./components/file-modal/file-modal"
+import { FooterComponent } from "./components/footer/footer"
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NoteModalComponent , FileModalComponent],
+  imports: [RouterOutlet, HeaderComponent, NoteModalComponent , FileModalComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ["./app.scss"],
 })
